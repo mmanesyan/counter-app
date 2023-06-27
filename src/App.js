ea32552eb03e90628ec312587e4b474bd71bfd08
Mane Manesyan <manemanesyan062@gmail.com>
2023-06-27 12:23:08 -0700

    Add the App function .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [count, setCount] = useState(0)
+  const [maxValue, setMaxValue] = useState(100)
+  const [minValue, setMinValue] = useState(-100)
+  const [step, setStep] = useState(1)
+
+  const Increase = () => {
+    if (count + step <= maxValue) {
+      setCount(count + step);
+    }
+  }
+  const Decrease = () => {
+    if (count - step >= minValue) {
+      setCount(count - step)
+    }
+  }
+  const Reset = () => {
+    setCount(0)
+  }
+  const MaxValueChange = (e) => {
+    setMaxValue(parseInt(e.target.value))
+  }
+  const MinValueChange = (e) => {
+    setMinValue(parseInt(e.target.value))
+  }
+  const StepChange = (e) => {
+    setStep(parseInt(e.target.value))
+  }
+
+ 
 }
 
 export default App;
