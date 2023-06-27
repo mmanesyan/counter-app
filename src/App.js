8fc850ed64c9d550708491c83c3108a5e3dd1f03
Mane Manesyan <manemanesyan062@gmail.com>
2023-06-27 23:53:10 +0400

    Add function App.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import React, { useState } from 'react'
-
 function App() {
   const [count, setCount] = useState(0)
   const [maxValue, setMaxValue] = useState(100)
   const [minValue, setMinValue] = useState(-100)
   const [step, setStep] = useState(1)
-
-  const Increase = () => {
-    if (count + step <= maxValue) {
-      setCount(count + step);
-    }
-  }
-  const Decrease = () => {
-    if (count - step >= minValue) {
-      setCount(count - step)
-    }
-  }
-  const Reset = () => {
-    setCount(0)
-  }
-  const MaxValueChange = (e) => {
-    setMaxValue(parseInt(e.target.value))
-  }
-  const MinValueChange = (e) => {
-    setMinValue(parseInt(e.target.value))
-  }
-  const StepChange = (e) => {
-    setStep(parseInt(e.target.value))
-  }
- 
+  
   return (
     <div>
       <h1>Counter App</h1>
@@ -55,4 +31,4 @@ function App() {
     </div>
   )
 }
-export default App;
+export default App
