2233ad63773e23bab5d6f4e060366999b7bb8bc1
Mane Manesyan <manemanesyan062@gmail.com>
2023-06-27 12:26:13 -0700

    Add the return part of code.

--- src/App.js
+++ src/App.js
@@ -28,8 +28,31 @@ function App() {
   const StepChange = (e) => {
     setStep(parseInt(e.target.value))
   }
-
  
-}
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={Increase}>Increase</button>
+      <button onClick={Decrease} disabled={count <= minValue}>Decrease</button>
+      <button onClick={Reset}>Reset</button>
 
+      <h2>Set Maximal and Minimal values for  Counter </h2>
+      <label>
+        Max Value:
+        <input type="number" value={maxValue} onChange={MaxValueChange} />
+      </label>
+      <br />
+      <label>
+        Min Value:
+        <input type="number" value={minValue} onChange={MinValueChange} />
+      </label>
+      <br />
+      <label>
+        Step:
+        <input type="number" value={step} onChange={StepChange} />
+      </label>
+    </div>
+  )
+}
 export default App;
